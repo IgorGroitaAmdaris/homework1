cdd44b9d94ce0e90551c48e0e3c3103ba062e131
igor.groita <igor.groita@MDDSK40264>
2021-02-16 13:21:17 +0200

    Just created the project.

---
+++ messageProject/messageProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace messageProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("It would be nice to compute some factorials!");
+            
+            int factorial(int n)
+            {
+                int p = 1;
+                for (int i = 1; i <= n; ++i)
+                {
+                    p *= i;
+                }
+                return p;
+            }
+        }
+
+        int factorial(int n)
+        {
+            int p = 1;
+            for(int i = 1; i<=n; ++i)
+            {
+                p *= i;
+            }
+            return p;
+        }
+    }
+}
